d1d1fe5b33bc34bdf4bcbab891eee83310ebd4c6
toge510 <wr9wu8h8@gmail.com>
2025-07-22 01:36:22 +0900

    Modify README.md

--- handson/58_VPC_Endpoint_for_S3/terraform/ec2.tf
+++ handson/58_VPC_Endpoint_for_S3/terraform/ec2.tf
@@ -4,6 +4,9 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.private_subnet_for_demo_ec2_private_subnet.id
   vpc_security_group_ids  = [aws_security_group.sg_ec2.id]
   iam_instance_profile    = aws_iam_instance_profile.ec2s3fullpermissionforendpointdemo.name
+  tags = {
+    Name = "ec2demo"
+  }
 }
 
 data "aws_ami" "amazon_linux" {
